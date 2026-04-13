a3c3dbb22e893e5825a2eeb76806c96e8adefbc5
Chi Hin <chanchihin_1@hotmail.com>
2026-04-14 00:09:56 +0800

    Refactored heap and stack benchmarking into a single serial.cpp

--- Makefile
+++ Makefile
@@ -1,22 +1,13 @@
 CPP = g++
 SRC = serial.cpp 
-SRC_STK = serial-stack.cpp
-OBJ = $(SRC:.cpp=.o)
-OBJ_STK = $(SRC_STK:.cpp:.o)
 
-all: serial serial-stack clean
+all: serial clean
 
 serial: $(OBJ)
 	$(CPP) $(OBJ) -o serial
 
 serial.o: $(SRC)
 	$(CPP) -c $(SRC) -o $(OBJ)
-
-serial-stack: $(OBJ_STK)
-	$(CPP) $(OBJ_STK) -o serial-stack
-
-serial-stack.o: $(SRC_STK)
-	$(CPP) -c $(SRC_STK) -o $(OBJ_STK)
 
 clean:
 	rm $(OBJ)
--- serial.cpp
+++ serial.cpp
@@ -1,9 +1,11 @@
 #include <time.h>
 #include <iostream>
 #include <chrono>
+#include <fstream>
+#include <iomanip>
 
 // Initialise
-void innit_vec(int* a, int* b, int* c, int N, double* t, int B)
+void innit_vec(int* a, int* b, int* c, int N, double* t_init, double* t_comp, int B)
 {
     for (int i = 0; i < N; ++i)
     {
@@ -14,7 +16,8 @@ void innit_vec(int* a, int* b, int* c, int N, double* t, int B)
 
     for (int i = 0; i < B; ++i)
     {
-        t[i] = 0.0;
+        t_init[i] = 0.0;
+        t_comp[i] = 0.0;
     }
 }
 void vector_add(int* a, int* b, int* c, int N)
@@ -25,41 +28,159 @@ void vector_add(int* a, int* b, int* c, int N)
     }
 } 
 
+void benchmark_allocate_heap(double* t, int N, int B)
+{
+
+	double avrg_t = 0.0;
+
+	for (int i = 0; i < B; ++i)
+	{
+		auto start = std::chrono::steady_clock::now();
+
+		int *a = new int[N];
+		int *b = new int[N];
+		int *c = new int[N];
+
+		for (int j = 0; j < N; ++j) a[j] = 1, b[j] = 2, c[j] = 0;
+
+		delete[] a;
+		delete[] b;
+		delete[] c;
+
+		auto end = std::chrono::steady_clock::now();
+		auto diff = end - start;
+
+		t[i] = std::chrono::duration<double, std::milli>(diff).count();
+        avrg_t += t[i];
+	}
+
+	std::cout << "Average time taken for heap allocation: " << (double)(avrg_t / B) << "ms" << std::endl;
+}
+
+void benchmark_allocate_stack(double* t, int N, int B)
+{
+	double avrg_t = 0.0;
+
+	for (int i = 0; i < B; ++i)
+	{
+		auto start = std::chrono::steady_clock::now();
+		int a[N];
+		int b[N];
+		int c[N];
+
+		for (int j = 0; j < N; ++j) a[j] = 1, b[j] = 2, c[j] = 0;
+
+		auto end = std::chrono::steady_clock::now();
+		auto diff = end - start;
+
+		t[i] = std::chrono::duration<double, std::milli>(diff).count();
+        avrg_t += t[i];
+	}
+
+	std::cout << "Average time taken for stack allocation: " << (double)(avrg_t / B) << "ms" << std::endl;
+}
+
+void benchmark_compute_stack(double* t, int N, int B)
+{
+	double avrg_t = 0.0;
+
+	int a[N];
+	int b[N];
+	int c[N];
+
+	for (int j = 0; j < N; ++j) a[j] = 1, b[j] = 2, c[j] = 0;
+
+	for (int i = 0; i < B; ++i)
+	{
+		auto start = std::chrono::steady_clock::now();
+		vector_add(a, b, c, N);
+		auto end = std::chrono::steady_clock::now();
+		auto diff = end - start;
+
+		t[i] = std::chrono::duration<double, std::milli>(diff).count();
+        avrg_t += t[i];
+	}
+
+	std::cout << "Average time taken for stack compute: " << (double)(avrg_t / B) << "ms" << std::endl;
+}
+
+void benchmark_compute_heap(double* t, int N, int B)
+{
+	double avrg_t = 0.0;
+
+	int* a = new int[N];
+	int* b = new int[N];
+	int* c = new int[N];
+
+	for (int j = 0; j < N; ++j) a[j] = 1, b[j] = 2, c[j] = 0;
+
+	for (int i = 0; i < B; ++i)
+	{
+		auto start = std::chrono::steady_clock::now();
+		vector_add(a, b, c, N);
+		auto end = std::chrono::steady_clock::now();
+		auto diff = end - start;
+
+		t[i] = std::chrono::duration<double, std::milli>(diff).count();
+        avrg_t += t[i];
+	}
+
+	delete[] a;
+	delete[] b;
+	delete[] c;
+
+	std::cout << "Average time taken for heap compute: " << (double)(avrg_t / B) << "ms" << std::endl;
+}
+
+void save_time(double* time, int B, const std::string& name)
+{
+	
+	std::string fname = name + ".txt";
+	std::ofstream outFile(fname);
+	
+	if (outFile.is_open())
+	{
+		for (int i = 0; i < B; ++i)
+		{
+			outFile << time[i] << std::endl;
+		}
+		outFile.close();
+	}
+	else
+	{
+		std::cerr << "Unable to open file" << std::endl;
+	}
+}
+	
 int main(int argc, char** argv)
 {
     int N = std::stoll(argv[1]);
     int B = std::stoi(argv[2]);
+	
+    std::cout << "--- Benchmarking vector addition of size " << N << ", over " << B << " loops ---" << std::endl;
 
-    int* a = new int[N];
-    int* b = new int[N];
-    int* c = new int[N];
-    double* t = new double[B];
-    double  t_avrg = 0.0;
+	double* init_heap	= new double[B];	
+	double* init_stack	= new double[B];
 
-    // Initialise vectors
-    std::cout << "--- Benchmarking vector addition of size " << N << " ---" << std::endl;
-    auto start = std::chrono::steady_clock::now();
-    innit_vec(a, b, c ,N, t, B);
-    auto end = std::chrono::steady_clock::now();
-    auto diff = end - start;
-    std::cout << "[Initialise] Time taken: " << std::chrono::duration<double, std::milli>(diff).count() << "ms" << std::endl;
+	benchmark_allocate_heap(init_heap, N, B);
+	benchmark_allocate_stack(init_stack, N, B);
+	
+	std::string allocate_heap_name = "heap_allocate";
+	save_time(init_heap, B, allocate_heap_name);
 
-    std::cout << "Running benchmark addition over " << B << " loops." << std::endl;
-    for (int i = 0; i < B; ++i)
-    {
-        start = std::chrono::steady_clock::now();
-        vector_add(a, b, c ,N);
-        end = std::chrono::steady_clock::now();
-        diff = end - start;
-        t[i] = std::chrono::duration<double, std::milli>(diff).count();
-        t_avrg += t[i];
-        // std::cout << "[Compute] Time taken: " << t[i] << "ms" << std::endl;
-    }
-    std::cout << std::endl;
-    std::cout << "[Compute] Average Time taken: " << (double)(t_avrg / B) << "ms" << std::endl;
+	std::string allocate_stack_name = "stack_allocate";
+	save_time(init_stack, B, allocate_stack_name);
 
-    delete[] a;
-    delete[] b;
-    delete[] c;
-    delete[] t;
+	benchmark_compute_heap(init_heap, N, B);
+	benchmark_compute_stack(init_stack, N, B);
+	
+	std::string compute_heap_name = "heap_compute";
+	save_time(init_heap, B, compute_heap_name);
+
+	std::string compute_stack_name = "stack_compute";
+	save_time(init_stack, B, compute_stack_name);
+
+	delete[] init_heap;
+	delete[] init_stack;
+
 }
